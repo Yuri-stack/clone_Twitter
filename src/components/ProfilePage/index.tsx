9e06608370ed68a7214e465b8854b9af28f48879
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2020-07-22 21:39:40 -0300

    Criando os primeiros Componentes e Estilos

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { Container, Banner, Avatar, ProfileData, LocationIcon, CakeIcon, Followage } from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+      <Container>
+          <Banner>
+              <Avatar />
+          </Banner>
+
+          <ProfileData>
+              {/* <EditButton outlined>Editar Perfil</EditButton> */}
+
+              <h1>Yuri Oliveira</h1>
+              <h2>@yuri_oliver</h2>
+
+              <p>   
+                Desenvolvedor Front-End
+              </p>
+
+              <ul>
+                  <li>
+                      <LocationIcon />
+                      São Paulo, Brasil
+                  </li>
+                  <li>
+                      <CakeIcon />
+                      Nascido(a) em 05 de Março de 1999
+                  </li>
+              </ul>
+
+              <Followage>
+                  <span>
+                      Seguindo <strong>05</strong>
+                  </span>
+                  <span>
+                    <strong>512 </strong> seguidores 
+                  </span>
+              </Followage>
+
+          </ProfileData>
+      </Container>
+  );
+}
+
+export default ProfilePage;
